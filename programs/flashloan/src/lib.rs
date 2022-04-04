4dd48d038240735e9a20453b493e76f6cf5586d8
Tengiz Sharafiev <btolfa@gmail.com>
2022-04-04 13:40:52 +0300

    Add Initialize, AddPool instruction and tests

--- programs/flashloan/src/lib.rs
+++ programs/flashloan/src/lib.rs
@@ -1,4 +1,5 @@
 use anchor_lang::prelude::*;
+use anchor_spl::token::{self, Mint, TokenAccount, MintTo, Burn, Transfer, Token};
 
 declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
 
@@ -7,9 +8,171 @@ pub mod flashloan {
     use super::*;
 
     pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
+        let flashloan = &mut ctx.accounts.flashloan;
+
+        flashloan.token_authority_bump = *ctx.bumps.get("token_authority").unwrap();
+        flashloan.authority = ctx.accounts.authority.key();
+
+        Ok(())
+    }
+
+    /// Add pool for a given token mint, setup a pool, token account and lp token mint
+    pub fn add_pool(ctx: Context<AddPool>) -> Result<()> {
+        let pool = &mut ctx.accounts.pool;
+
+        pool.bump = *ctx.bumps.get("pool").unwrap();
+        pool.borrowing = false;
+        pool.token_mint = ctx.accounts.token_mint.key();
+        pool.pool_token = ctx.accounts.pool_token.key();
+        pool.lp_token_mint = ctx.accounts.lp_token_mint.key();
+
+        Ok(())
+    }
+
+    /// Receive tokens and mint lp tokens
+    pub fn deposit(ctx: Context<Deposit>, amount: u64) -> Result<()> {
+        Ok(())
+    }
+
+    pub fn withdraw(ctx: Context<Withdraw>, amount: u64) -> Result<()> {
+        Ok(())
+    }
+
+    pub fn mint_voucher(ctx: Context<MintVoucher>) -> Result<()> {
+        Ok(())
+    }
+
+    pub fn borrow(ctx: Context<Borrow>, amount: u64) -> Result<()> {
+        Ok(())
+    }
+
+    pub fn repay(ctx: Context<Repay>, amount: u64) -> Result<()> {
         Ok(())
     }
 }
 
+// ----------------------------------------------------------------------------
+
+pub const FLASHLOAN_NAMESPACE: [u8; 9] = *b"flashloan";
+pub const TOKEN_NAMESPACE: [u8; 5] = *b"token";
+pub const LP_TOKEN_NAMESPACE: [u8; 14] = *b"liquidity_pool";
+
+// ----------------------------------------------------------------------------
+
 #[derive(Accounts)]
-pub struct Initialize {}
+pub struct Initialize<'info> {
+    #[account(
+        init,
+        payer = payer,
+        space = FlashLoan::LEN,
+    )]
+    pub flashloan: Account<'info, FlashLoan>,
+
+    #[account(
+        seeds = [flashloan.key().as_ref(), FLASHLOAN_NAMESPACE.as_ref()],
+        bump
+    )]
+    /// CHECK: Checked above, used only for bump calc
+    pub token_authority: UncheckedAccount<'info>,
+
+    /// CHECK: This is not dangerous because we don't read or write from this account
+    pub authority: UncheckedAccount<'info>,
+
+    #[account(mut)]
+    pub payer: Signer<'info>,
+
+    pub system_program: Program<'info, System>,
+}
+
+#[derive(Accounts)]
+pub struct AddPool<'info> {
+    #[account(has_one = authority)]
+    pub flashloan: Box<Account<'info, FlashLoan>>,
+
+    #[account(
+        seeds = [flashloan.key().as_ref(), FLASHLOAN_NAMESPACE.as_ref()],
+        bump = flashloan.token_authority_bump
+    )]
+    /// CHECK: Checked above, used only for bump calc
+    pub token_authority: UncheckedAccount<'info>,
+
+    pub authority: Signer<'info>,
+
+    #[account(mut)]
+    pub payer: Signer<'info>,
+
+    #[account(
+        init,
+        payer = payer,
+        space = Pool::LEN,
+        seeds = [flashloan.key().as_ref(), token_mint.key().as_ref()],
+        bump,
+    )]
+    pub pool: Box<Account<'info, Pool>>,
+
+    #[account(
+        init,
+        seeds = [flashloan.key().as_ref(), TOKEN_NAMESPACE.as_ref(), token_mint.key().as_ref()],
+        bump,
+        token::mint = token_mint,
+        token::authority = token_authority,
+        payer = payer,
+    )]
+    pub pool_token: Account<'info, TokenAccount>,
+
+    #[account(
+        init,
+        seeds = [flashloan.key().as_ref(), LP_TOKEN_NAMESPACE.as_ref(), token_mint.key().as_ref()],
+        bump,
+        mint::authority = token_authority,
+        mint::decimals = token_mint.decimals,
+        payer = payer,
+    )]
+    pub lp_token_mint: Account<'info, Mint>,
+
+    pub token_mint: Account<'info, Mint>,
+
+    pub token_program: Program<'info, Token>,
+    pub rent: Sysvar<'info, Rent>,
+    pub system_program: Program<'info, System>,
+}
+
+#[derive(Accounts)]
+pub struct Deposit {
+}
+
+#[derive(Accounts)]
+pub struct Withdraw {
+}
+
+#[derive(Accounts)]
+pub struct MintVoucher {}
+
+#[derive(Accounts)]
+pub struct Borrow {}
+
+#[derive(Accounts)]
+pub struct Repay {}
+
+#[account]
+pub struct FlashLoan {
+    token_authority_bump: u8,
+    authority: Pubkey,
+}
+
+impl FlashLoan {
+    const LEN: usize = 8 + 32 + 1;
+}
+
+#[account]
+pub struct Pool {
+    bump: u8,
+    borrowing: bool,
+    token_mint: Pubkey,
+    pool_token: Pubkey,
+    lp_token_mint: Pubkey,
+}
+
+impl Pool {
+    const LEN: usize = 8 + 2 + 32*3;
+}
--- tests/flashloan.ts
+++ tests/flashloan.ts
@@ -1,16 +1,141 @@
 import * as anchor from "@project-serum/anchor";
-import { Program } from "@project-serum/anchor";
+import { Program, web3, BN, Spl } from "@project-serum/anchor";
+import {PublicKey, Keypair} from '@solana/web3.js';
 import { Flashloan } from "../target/types/flashloan";
+
+import { expect } from 'chai';
+import * as chai from 'chai';
+import chaiAsPromised from 'chai-as-promised';
+chai.use(chaiAsPromised);
 
 describe("flashloan", () => {
   // Configure the client to use the local cluster.
   anchor.setProvider(anchor.Provider.env());
-
+  const provider = anchor.getProvider();
   const program = anchor.workspace.Flashloan as Program<Flashloan>;
+  const spl_token = Spl.token();
 
-  it("Is initialized!", async () => {
-    // Add your test here.
-    const tx = await program.rpc.initialize({});
-    console.log("Your transaction signature", tx);
+  const flashloan = Keypair.generate();
+  const authority = Keypair.generate();
+
+  const mint = Keypair.generate();
+  const token1 = Keypair.generate();
+
+  async function create_mint(mint: Keypair, mint_authority: PublicKey) {
+    await spl_token.methods
+        .initializeMint(9, mint_authority, null)
+        .accounts({
+          mint: mint.publicKey,
+          rent: web3.SYSVAR_RENT_PUBKEY,
+        })
+        .signers([mint])
+        .preInstructions([await spl_token.account.mint.createInstruction(mint)])
+        .rpc();
+  }
+
+  async function create_token(token: Keypair, mint: PublicKey, authority: PublicKey) {
+    await spl_token.methods.initializeAccount()
+        .accounts({
+          account: token.publicKey,
+          mint: mint,
+          authority: authority,
+          rent: web3.SYSVAR_RENT_PUBKEY,
+        })
+        .signers([token])
+        .preInstructions([await spl_token.account.token.createInstruction(token)])
+        .rpc();
+  }
+
+  async function find_token_authority(flashloan: PublicKey) {
+    return await PublicKey.findProgramAddress(
+      [flashloan.toBuffer(),
+        Buffer.from(anchor.utils.bytes.utf8.encode("flashloan"))],
+      program.programId
+    );
+  }
+
+  async function find_pool(flashloan: PublicKey, mint: PublicKey) {
+    const [pool, _nonce] = await PublicKey.findProgramAddress(
+      [flashloan.toBuffer(), mint.toBuffer()],
+      program.programId
+    );
+
+    return pool;
+  }
+
+  async function find_pool_token(flashloan: PublicKey, mint: PublicKey) {
+    const [pool_token, _nonce] = await PublicKey.findProgramAddress(
+      [flashloan.toBuffer(), Buffer.from(anchor.utils.bytes.utf8.encode("token")), mint.toBuffer()],
+      program.programId
+    );
+
+    return pool_token;
+  }
+
+  async function find_lp_token_mint(flashloan: PublicKey, mint: PublicKey) {
+    const [lp_token_mint, _nonce] = await PublicKey.findProgramAddress(
+      [flashloan.toBuffer(), Buffer.from(anchor.utils.bytes.utf8.encode("liquidity_pool")), mint.toBuffer()],
+      program.programId
+    );
+
+    return lp_token_mint;
+  }
+
+  before(async () => {
+    await create_mint(mint, provider.wallet.publicKey);
+    await create_token(token1, mint.publicKey, provider.wallet.publicKey);
+  })
+
+  it("Should initialize flashloan", async () => {
+    await program.methods
+      .initialize()
+      .accounts({
+        flashloan: flashloan.publicKey,
+        authority: authority.publicKey,
+      })
+      .signers([flashloan])
+      .rpc();
+
+    const flashloanAccount = await program.account.flashLoan.fetch(flashloan.publicKey);
+    expect(flashloanAccount.authority).to.be.deep.equal(authority.publicKey);
+
+    const [token_authority, bump] = await find_token_authority(flashloan.publicKey);
+    expect(flashloanAccount.tokenAuthorityBump).to.be.equal(bump);
+  });
+
+  it("Should add pool", async () => {
+    await program.methods
+      .addPool()
+      .accounts({
+        flashloan: flashloan.publicKey,
+        authority: authority.publicKey,
+        tokenMint: mint.publicKey,
+      })
+      .signers([authority])
+      .rpc();
+
+    const poolAccount = await program.account.pool.fetch(await find_pool(flashloan.publicKey, mint.publicKey));
+    const poolTokenAccount = await spl_token.account.token.fetch(await find_pool_token(flashloan.publicKey, mint.publicKey));
+    const lpTokenMintAccount = await spl_token.account.mint.fetch(await find_lp_token_mint(flashloan.publicKey, mint.publicKey));
+    const mintAccount = await spl_token.account.mint.fetch(mint.publicKey);
+
+    const [token_authority, _nonce] = await find_token_authority(flashloan.publicKey);
+
+    expect(poolAccount.borrowing).to.be.false;
+    expect(poolAccount.tokenMint).to.be.deep.equal(mint.publicKey);
+    expect(poolAccount.poolToken).to.be.deep.equal(await find_pool_token(flashloan.publicKey, mint.publicKey));
+    expect(poolAccount.lpTokenMint).to.be.deep.equal(await find_lp_token_mint(flashloan.publicKey, mint.publicKey))
+
+    expect(poolTokenAccount.mint).to.be.deep.equal(mint.publicKey);
+    expect(poolTokenAccount.authority).to.be.deep.equal(token_authority);
+    expect(poolTokenAccount.amount.toNumber()).to.be.equal(0);
+
+    expect(lpTokenMintAccount.mintAuthority).to.be.deep.equal(token_authority);
+    expect(lpTokenMintAccount.supply.toNumber()).to.be.equal(0);
+    expect(lpTokenMintAccount.decimals).to.be.equal(mintAccount.decimals);
+
+    // pool
+    // pool_token
+    // lp_token_mint
   });
 });
